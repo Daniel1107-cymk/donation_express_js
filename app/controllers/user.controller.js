4ad9e32fb96aa327e08243d1111fba2543f05a5d
Daniel Lim <daniel@grip-principle.com>
2021-07-21 16:35:57 +0700

    revise user model and create function

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -2,17 +2,9 @@ const User = require("../models/user.model");
 
 // create
 exports.create = (req, res) => {
-  const userData = new User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    address: req.body.address,
-    phoneNumber: req.body.phoneNumber,
-  });
-
-  User.create(userData, (err, data) => {
-    if (err) {
-      res.status(500).send(err.error);
-    } else res.send(data);
+  User.create(req.body, (err, data) => {
+    if (err) return res.status(500).send(err.error);
+    return res.send(data);
   });
 };
 
@@ -20,19 +12,16 @@ exports.create = (req, res) => {
 exports.findById = (req, res) => {
   User.selectById(req.params.userId, (err, data) => {
     if (err) {
-      if (err.find === "not found") {
-        res.status(404).send(err);
-        return;
-      }
-      res.status(500).send(err);
-    } else res.send(data);
+      if (err.find === "not found") return res.status(404).send(err);
+      return res.status(500).send(err);
+    }
+    return res.send(data);
   });
 };
 
 exports.selectAll = (req, res) => {
   User.selectAll((err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else res.send(data);
+    if (err) return res.status(500).send(err);
+    return res.send(data);
   });
 };
--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,73 +1,65 @@
 const mongoose = require("mongoose");
+const responseFormat = require("../helpers/response.format");
 
 const userSchema = mongoose.Schema({
-  firstName: {
+  first_name: {
     type: String,
     required: true,
   },
-  lastname: {
+  last_name: {
     type: String,
-    required: false,
   },
-  address: {
-    type: String,
-    required: true,
-  },
-  phoneNumber: {
+  full_address: {
     type: String,
     required: true,
   },
-  googleId: String,
+  phone_number: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: 0,
+  },
+  google_id: {
+    type: String,
+  },
 });
 
 const User = mongoose.model("users", userSchema);
 
-const responseFormat = (data, success) => {
-  if (success) {
-    return {
-      data: data,
-      success: true,
-    };
-  } else {
-    return {
-      error: data,
-      success: false,
-    };
-  }
-};
-
-User.create = (userData, result) => {
+User.create = (data, result) => {
   const user = new User({
-    firstName: userData.firstName,
-    lastName: userData.lastName,
-    address: userData.address,
-    phoneNumber: userData.phoneNumber,
-    googleId: userData.googleId,
+    first_name: data.first_name,
+    last_name: data.last_name,
+    full_address: data.full_address,
+    phone_number: data.phone_number,
+    google_id: data.google_id,
   });
 
   user
     .save()
     .then((createdUser) => {
-      result(null, responseFormat(createdUser, true));
+      return result(null, responseFormat.format(createdUser, true));
     })
     .catch((err) => {
-      result(responseFormat(err, false), null);
+      return result(responseFormat.format(err, false), null);
     });
 };
 
 User.selectById = (userId, result) => {
   User.findById(userId, (err, user) => {
-    if (err) return result(responseFormat(err, false), null);
+    if (err) return result(responseFormat.format(err, false), null);
 
-    return result(null, responseFormat(user, true));
+    return result(null, responseFormat.format(user, true));
   });
 };
 
 User.selectAll = (result) => {
   User.find({}, (err, user) => {
-    if (err) return result(responseFormat(err, false), null);
+    if (err) return result(responseFormat.format(err, false), null);
 
-    return result(null, responseFormat(user, true));
+    return result(null, responseFormat.format(user, true));
   });
 };
 
