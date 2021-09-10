8dde4706de8265253d6526fd15949db8ddf42b27
Daniel Lim <daniel@grip-principle.com>
2021-09-10 21:01:11 +0700

    refactor user login and register

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,123 +1,19 @@
 const mongoose = require("mongoose");
 const responseFormat = require("../helpers/response.format");
 const byscrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const userSchema = mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  first_name: {
-    type: String,
-    required: true,
-  },
-  last_name: {
-    type: String,
-  },
-  full_address: {
-    type: String,
-    required: true,
-  },
-  phone_number: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    default: 0,
-  },
-  google_id: {
-    type: String,
-  },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String },
+  full_address: { type: String, required: true },
+  phone_number: { type: String, required: true },
+  role: { type: String, default: 0 },
+  google_id: { type: String },
+  token: { type: String },
 });
 
-const User = mongoose.model("users", userSchema);
-
-let UserModel = {
-  create: (userData, result) => {
-    const user = new User({
-      email: userData.email,
-      password: byscrypt.hashSync(userData.password, 10),
-      first_name: userData.first_name,
-      last_name: userData.last_name,
-      phone_number: userData.phone_number,
-      full_address: userData.full_address,
-      role: userData.role,
-      google_id: userData.google_id,
-    });
-    user
-      .save()
-      .then((createdUser) => {
-        return result(null, responseFormat.format(createdUser, true));
-      })
-      .catch((err) => {
-        return result(responseFormat.format(err, false), null);
-      });
-  },
-
-  login: (userData, result) => {
-    User.findOne(
-      {
-        email: userData.email,
-      },
-      "-_id -__v"
-    )
-      .then(async (user) => {
-        if (user === null) {
-          const err = [
-            { msg: "User not found", param: "email", location: "body" },
-          ];
-          return result(responseFormat.format(err, false), null);
-        } else {
-          const match = byscrypt.compareSync(userData.password, user.password);
-          if (match) {
-            return result(null, responseFormat.format(user, true));
-          } else {
-            const err = [
-              {
-                msg: "Incorrect password",
-                param: "password",
-                location: "body",
-              },
-            ];
-            return result(responseFormat.format(err, false), null);
-          }
-        }
-      })
-      .catch((err) => {
-        return result(responseFormat.format(err, false), null);
-      });
-  },
-
-  updateProfile: (userData, result) => {
-    User.findByIdAndUpdate(userData.id, userData, { new: true })
-      .then((updatedProfile) => {
-        return result(null, responseFormat.format(updatedProfile, true));
-      })
-      .catch((err) => {
-        return result(responseFormat.format(err, false), null);
-      });
-  },
-
-  selectById: (userId, result) => {
-    User.findById(userId, (err, user) => {
-      if (err) return result(responseFormat.format(err, false), null);
-
-      return result(null, responseFormat.format(user, true));
-    });
-  },
-
-  selectAll: (result) => {
-    User.find({}, "-_id -__v", (err, user) => {
-      if (err) return result(responseFormat.format(err, false), null);
-
-      return result(null, responseFormat.format(user, true));
-    });
-  },
-};
-
-module.exports = UserModel;
+module.exports = mongoose.model("users", userSchema);
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,20 +1,117 @@
+const byscrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 // env
 require("dotenv").config();
-// api prefix
 const apiPrefix = process.env.API_PREFIX;
-// validator
+// middleware
+const auth = require("../middleware/auth");
+// helper
 const validate = require("../validator/auth");
+const responseFormat = require("../helpers/response.format");
 
 module.exports = (app) => {
-  const users = require("../controllers/user.controller.js");
+  const User = require("../models/user.model");
+
   // login
-  app.post(`${apiPrefix}/login`, validate.login, users.login);
+  app.post(`${apiPrefix}/login`, validate.login, async (req, res) => {
+    try {
+      const body = req.body;
+
+      const user = await User.findOne({ email: body.email }, "-_id -__v");
+      if (user && byscrypt.compareSync(body.password, user.password)) {
+        // create token
+        const { email } = body;
+        const token = jwt.sign(
+          { user_id: user._id, email },
+          process.env.TOKEN_SECRET,
+          { expiresIn: "1 day" }
+        );
+        // save user token
+        user.token = token;
+        res.status(200).json(responseFormat.format(user, true));
+      } else {
+        const err = [
+          {
+            msg: "Invalid credentials",
+            param: "password",
+            location: "body",
+          },
+        ];
+        return res.status(400).send(responseFormat.format(err, false));
+      }
+    } catch (err) {
+      return res.status(500).json(responseFormat.format(err, false));
+    }
+  });
+
   // create a new user
-  app.post(`${apiPrefix}/register`, validate.signup, users.create);
+  app.post(`${apiPrefix}/register`, validate.signup, async (req, res) => {
+    try {
+      const body = req.body;
+      // check if user exist
+      const oldUser = await User.findOne({ email: body.email });
+      if (oldUser) {
+        const err = [
+          {
+            msg: "Email already exist",
+            param: "password",
+            location: "body",
+          },
+        ];
+        return res.status(409).send(responseFormat.format(err, false));
+      }
+      // create user
+      const user = await User.create({
+        email: body.email,
+        password: byscrypt.hashSync(body.password, 10),
+        first_name: body.first_name,
+        last_name: body.last_name,
+        phone_number: body.phone_number,
+        full_address: body.full_address,
+        role: body.role,
+        google_id: body.google_id,
+      });
+      // create token
+      const { email } = body;
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_SECRET,
+        { expiresIn: "1 day" }
+      );
+      // save user token
+      user.token = token;
+
+      return res.status(200).json(responseFormat.format(user, true));
+    } catch (err) {
+      return res.status(500).json(responseFormat.format(err, false));
+    }
+  });
   // update profile
-  app.put(`${apiPrefix}/update-profile/:userId`, users.updateProfile);
+  app.put(`${apiPrefix}/update-profile/:userId`, auth, async (req, res) => {
+    User.findByIdAndUpdate(req.params.userId, req.body, { new: true })
+      .then((updatedProfile) => {
+        return res
+          .status(200)
+          .json(responseFormat.format(updatedProfile, true));
+      })
+      .catch((err) => {
+        return res.status(500).json(responseFormat.format(err, false));
+      });
+  });
   // find single user by id
-  app.get(`${apiPrefix}/users/:userId`, users.findById);
+  app.get(`${apiPrefix}/users/:userId`, auth, async (req, res) => {
+    User.findById(req.params.userId, (err, user) => {
+      if (err) return res.status(500).json(responseFormat.format(err, false));
+
+      return res.status(200).json(responseFormat.format(user, true));
+    });
+  });
   // select all user
-  app.get(`${apiPrefix}/users`, users.selectAll);
+  app.get(`${apiPrefix}/users`, auth, async (req, res) => {
+    User.find({}, "-_id -__v", (err, user) => {
+      if (err) return res.status(200).json(responseFormat.format(err, false));
+
+      return res.status(200).json(responseFormat.format(user, true));
+    });
+  });
 };
