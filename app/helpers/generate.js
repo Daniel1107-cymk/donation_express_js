e8ceec0c89dca11f0dfba9c29ddd077110d9575b
Daniel Lim <daniel@grip-principle.com>
2021-09-11 11:39:09 +0700

    add generate helper

---
+++ app/helpers/generate.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+exports.randomPassword = async (length) => {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+exports.token = async ({ userId, email }) => {
+  const token = jwt.sign(
+    { user_id: userId, email },
+    process.env.TOKEN_SECRECT,
+    { expiresIn: "1 day" }
+  );
+  return token;
+};
