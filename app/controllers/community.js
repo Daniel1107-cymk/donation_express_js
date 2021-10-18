b38217076509c06ff0c30de77a032d87f936d480
Daniel Lim <daniellim4400@gmail.com>
2021-10-18 19:17:35 +0700

    revise get all community able to set params

--- app/controllers/community.js
+++ app/controllers/community.js
@@ -24,7 +24,10 @@ const CommunityController = {
     }
   }),
   getAllCommunity: asyncWrap(async (req, res) => {
-    const community = await Community.find({}, "name banner mimetype");
+    const community =
+      req.query.max !== undefined
+        ? await Community.find({}, "name").limit(parseInt(req.query.max))
+        : await Community.find({}, "name banner mimetype");
     if (community) {
       return res.status(200).json(responseFormat.format(community, true));
     }
